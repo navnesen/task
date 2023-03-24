f79c08adc31b8e7135fe9373cf9ac27db82be7b1
Jameson <j4m350n@testersen.no>
2023-03-24 11:52:00 +0100

    Added call signatures for Task

---
+++ src/main/java/no/navnesen/Task.java
@@ -0,0 +1,36 @@
+package no.navnesen;
+
+import java.util.List;
+
+/**
+ * A quick and easy method for performing synchronous tasks in an asynchronous manner.
+ */
+public class Task<T> {
+	public static <T> Task<T> complete(T value) {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public static <T> Task<T> fail(Exception exception) {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public static <T> Task<List<T>> all(List<Task<T>> tasks) {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public T await() {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public <V> Task<V> and(TaskActionAnd<V, T> action) {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public <V> Task<V> map(TaskActionMap<V, T> action) {
+		throw new RuntimeException("Not implemented");
+	}
+
+	public Task<T> or(TaskActionOr<T> action) {
+		throw new RuntimeException("Not implemented");
+	}
+}
